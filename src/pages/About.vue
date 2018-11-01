c781bc4c4bfa62f1ab5353acd1f5df0d7d7db3c5
Konrad <konrad.trade@gmail.com>
2018-11-01 18:15:37 +0100

    Added about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about">    
+    <h1>Hey, my name's <a href="https://github.com/konradovsky" class="about__blue">Konrad</a></h1>
+    <p>I home this starter will help you with your clean style!</p>
   </div>
 </template>
+<style lang="scss">
+.about{
+  &__blue{
+    color: blue;
+  }
+}
+</style>
